d4053ced6b28c930f5099734e1905b777a748586
Rosan Shanmuganathan <145561869+na50r@users.noreply.github.com>
2025-08-14 15:57:36 +0200

    Snake go (#1)
    * smooth anim
    
    * snake logic
    
    * adapt movement
    
    * works
    
    * working single player snake
    
    * handle tail eating
    
    * fix up css
    
    * lower grid size
    
    * basic websocket setup
    
    * works
    
    * add server code
    
    * working multiplayer snake
    
    * set random start
    
    * fix up css

---
+++ main.js
@@ -0,0 +1,122 @@
+import { Input } from './scripts/input.js';
+import { Map } from './scripts/map.js';
+import { Snake } from './scripts/snake.js';
+
+const app = document.getElementById('app');
+const startBtn = document.createElement('button');
+const stopBtn = document.createElement('button');
+startBtn.innerText = 'Respawn';
+stopBtn.innerText = 'Pause';
+
+const canvas = document.createElement('canvas');
+
+canvas.id = 'canvasX';
+const ctx = canvas.getContext('2d');
+
+export const COLS = 32
+export const ROWS = 32
+export const TILE_SIZE = 16;
+export const GAME_WIDTH = COLS * TILE_SIZE;
+export const GAME_HEIGHT = ROWS * TILE_SIZE;
+
+canvas.width = GAME_WIDTH;
+canvas.height = GAME_HEIGHT;
+app.append(canvas, startBtn, stopBtn);
+
+function drawGrid(ctx) {
+    ctx.strokeStyle = 'white';
+    for (let c = 0; c < COLS; c++) {
+        for (let r = 0; r < ROWS; r++) {
+            ctx.strokeRect(c * TILE_SIZE, r * TILE_SIZE, TILE_SIZE, TILE_SIZE);
+        }
+    }
+}
+
+class Game {
+    constructor() {
+        this.over = false;
+        this.socket = new WebSocket('ws://localhost:8080/ws');
+        this.map = new Map(this);
+        this.input = new Input(this);
+        this.snake = new Snake(this);
+        this.debug = false;
+        this.socket.onmessage = (event) => {
+            const msg = JSON.parse(event.data);
+            if (msg.type === 'map') {
+                this.map.data = msg.payload;
+            }
+            if (msg.type === 'grow') {
+                this.snake.size += 1;
+            }
+            if (msg.type === "death") {
+                this.over = true;
+            }
+        };
+    }
+    toggleDebug() {
+        this.debug = !this.debug;
+    }
+
+    render(ctx, deltaTime) {
+        ctx.clearRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
+        if (this.debug) drawGrid(ctx);
+        this.snake.update(deltaTime);
+        this.map.draw(ctx);
+    };
+}
+
+function killGame(game) {
+    game.socket.close();
+    game.socket = null;
+    game = null;
+}
+
+
+var aniID;
+var lastTime = 0;
+var paused = false;
+var currentState = { game: null, loop: null }
+function createGameLoop() {
+    const game = new Game();
+    const gameLoop = (timeStamp) => {
+        aniID = requestAnimationFrame(gameLoop);
+        const deltaTime = timeStamp - lastTime;
+        lastTime = timeStamp;
+        game.render(ctx, deltaTime);
+        if (game.over) {
+            alert('You died!');
+            cancelAnimationFrame(aniID);
+            killGame(game);
+            window.location.reload();
+        }
+    };
+    return {game: game, loop: gameLoop };
+}
+
+window.addEventListener('load', () => {
+    currentState = createGameLoop();
+    requestAnimationFrame(currentState.loop);
+})
+
+startBtn.addEventListener('click', () => {
+    if (aniID) {
+        cancelAnimationFrame(aniID);
+    }
+    if (currentState.game) {
+        killGame(currentState.game);
+    }
+    currentState = createGameLoop();
+    requestAnimationFrame(currentState.loop);
+});
+stopBtn.addEventListener('click', () => {
+    if (paused) {
+        paused = false;
+        stopBtn.innerText = 'Pause';
+        aniID = requestAnimationFrame(currentState.loop);
+        return;
+    }
+    cancelAnimationFrame(aniID);
+    stopBtn.innerText = 'Resume';
+    paused = true;
+})
+
---
+++ server/main.go
@@ -0,0 +1,224 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+	"math/rand"
+)
+
+// Based on https://www.youtube.com/watch?v=y036l6pvVEs
+
+type Message struct {
+	Type string `json:"type"`
+	Payload interface{} `json:"payload"`
+}
+
+type Client struct {
+	socket  *websocket.Conn
+	receive chan []byte
+	room    *Room
+}
+
+func (c *Client) read() {
+    defer c.socket.Close()
+    for {
+        _, m, err := c.socket.ReadMessage()
+        if err != nil {
+            return
+        }
+		var msg Message
+        if err := json.Unmarshal(m, &msg); err != nil {
+			log.Println("Error unmarshalling message:", err)
+            continue
+        }
+		if msg.Type == "positions" {
+			raw := msg.Payload.([]interface{})
+			positions := make([]int, len(raw))
+			for i, v := range raw {
+				positions[i] = int(v.(float64)) 
+			}
+			c.room.update <- snakeUpdate{c, positions}
+		}
+    }
+}
+
+func (c *Client) write() {
+	defer c.socket.Close()
+	for msg := range c.receive {
+		err := c.socket.WriteMessage(1, msg)
+		if err != nil {
+			return
+		}
+	}
+}
+
+type Food struct {
+	position int
+	snakes map[*Client][]int
+}
+
+func (f *Food) generate(mapSize int) {
+	f.position = rand.Intn(mapSize)
+}
+
+func (f *Food) check(snakes map[*Client][]int, eaten chan *Client) {
+	for cli, positions := range snakes {
+		for _, idx := range positions {
+			if idx == f.position {
+				eaten <- cli
+			}
+		}
+	}
+}
+
+
+func checkDeath(snakes map[*Client][]int, death chan *Client) {
+	for cli, positions := range snakes {
+		head := positions[0]
+		for other, positions := range snakes {
+			if cli == other {
+				continue
+			}
+			for _, idx := range positions {
+				if idx == head {
+					death <- cli
+				}
+			}
+		}
+	}
+}
+
+func NewFood() *Food {
+	f := &Food{snakes: make(map[*Client][]int),
+		}
+	f.generate(32 * 32)
+	return f
+}
+
+type Room struct {
+	clients map[*Client]bool
+	snakes map[*Client][]int // positions of snakes on map
+	join    chan *Client
+	leave   chan *Client
+	forward chan []byte
+	update chan snakeUpdate
+	food *Food
+	eaten chan *Client
+	death chan *Client
+}
+
+type snakeUpdate struct {
+	client *Client
+	body []int
+}
+
+func NewRoom() *Room {
+	return &Room{
+		clients: make(map[*Client]bool),
+		join:    make(chan *Client),
+		leave:   make(chan *Client),
+		forward: make(chan []byte),
+		snakes: make(map[*Client][]int),
+		update: make(chan snakeUpdate),
+		food: NewFood(),
+		eaten: make(chan *Client),
+		death: make(chan *Client),
+	}
+}
+
+func drawMap(snakes map[*Client][]int, food *Food) []int {
+	gameMap := make([]int, 32 * 32)
+	for _, positions := range snakes {
+		for _, idx := range positions {
+			if idx >= 0 && idx < len(gameMap) {
+				gameMap[idx] = 1
+			}
+		}
+	}
+	gameMap[food.position] = 2
+	//log.Println(gameMap)
+	return gameMap
+}
+
+func (r *Room) run() {
+    for {
+        select {
+        case cli := <-r.join:
+			log.Println("New client joined")
+            r.clients[cli] = true
+
+        case cli := <-r.leave:
+			log.Println("Client left")
+            delete(r.clients, cli)
+            delete(r.snakes, cli)
+            close(cli.receive)
+
+        case upd := <-r.update:
+            r.snakes[upd.client] = upd.body
+			go r.food.check(r.snakes, r.eaten)
+			go checkDeath(r.snakes, r.death)
+            gameMap := drawMap(r.snakes, r.food)
+            data, _ := json.Marshal(Message{Type: "map", Payload: gameMap})
+            for cli := range r.clients {
+                select {
+                case cli.receive <- data:
+                default:
+                }
+            }
+		case cli := <-r.eaten:
+			log.Printf("Food eaten %v", cli)
+			r.food.generate(32 * 32)
+			data, _ := json.Marshal(Message{Type: "grow", Payload: nil})
+			select {
+				case cli.receive <- data:
+				default:
+			}
+		case cli := <-r.death:
+			log.Printf("Snake died %v", cli)
+			delete(r.snakes, cli)
+			data, _ := json.Marshal(Message{Type: "death", Payload: nil})
+			select {
+				case cli.receive <- data:
+				default:
+			}
+        }
+    }
+}
+
+
+var upgrader = &websocket.Upgrader{
+	ReadBufferSize: 1024, 
+	WriteBufferSize: 1024,
+	CheckOrigin: func(r *http.Request) bool { return true },
+}
+
+func (r *Room) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	//handler that maintains the websocket connection
+	socket, err := upgrader.Upgrade(w, req, nil)
+	if err != nil {
+		log.Fatal("ServeHTTP:", err)
+		return
+	}
+	client := &Client{
+		socket: socket, 
+		receive: make(chan []byte, 1024), 
+		room: r}
+	r.join <- client
+	defer func() { r.leave <- client }()
+	go client.write()
+	client.read()
+}
+
+
+func main() {
+	r := NewRoom()
+	go r.run()
+	http.Handle("/ws", r)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
+}
