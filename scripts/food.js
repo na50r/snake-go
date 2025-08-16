e3588c379afd33ec96753cbe4ad8072de6d6421a
na50r <shanrosan@gmail.com>
2025-08-16 11:25:34 +0200

    fix food gen

--- scripts/food.js
+++ scripts/food.js
@@ -2,15 +2,14 @@ import { TILE_SIZE } from "../main.js";
 import { ROWS, COLS } from "../main.js";
 import { GAME_WIDTH, GAME_HEIGHT } from "../main.js";
 
-function setRandomPosition(collisionLayer) {
-    const maxCol = COLS - 2;
-    const maxRow = ROWS - 2;
-    const minCol = 2;
-    const minRow = 2;
-    const col = Math.floor(Math.random() * (maxCol - minCol) + minCol);
-    const row = Math.floor(Math.random() * (maxRow - minRow) + minRow);
-    if (collisionLayer[COLS * row + col] === 2) {
-        return setRandomPosition(collisionLayer);
+function setRandomPosition(map) {
+    const maxCol = COLS - 1;
+    const maxRow = ROWS - 1;
+    const col = Math.floor(Math.random() * (maxCol));
+    const row = Math.floor(Math.random() * (maxRow));
+    const pos = COLS * row + col;
+    if (map.length > 0 && map[pos] === 1) {
+        return setRandomPosition(map);
     }
     return { x: col, y: row };
 }
@@ -19,12 +18,12 @@ export class Food {
     constructor(game) {
         this.game = game;
         this.eaten = 0;
-        this.pos = setRandomPosition([]);
+        this.pos = setRandomPosition(this.game.map.data);
         this.game.map.set(this.pos.x, this.pos.y, 2);
     }
     update() {
         console.log("food");
-        this.pos = setRandomPosition([]);
+        this.pos = setRandomPosition(this.game.map.data);
         this.game.snake.size += 1;
         this.eaten += 1;
         this.game.map.set(this.pos.x, this.pos.y, 2);
